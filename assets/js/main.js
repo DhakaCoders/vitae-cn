977f66c887b66933aa4d2f5800c191e3deecb49e
milon440 <milonsarker414@gmail.com>
2019-12-13 19:41:02 +0600

    header desktop view done

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+(function($) {
+
+$('.navbar-toggle').on('click', function(){
+	$('#mobile-nav').slideToggle(300);
+});
+	
+
+/**
+Responsive on 767px
+*/
+var windowWidth = $(window).width();
+// if (windowWidth <= 767) {
+
+  $('.toggle-btn').on('click', function(){
+    $(this).toggleClass('menu-expend');
+    $('.toggle-bar ul').slideToggle(500);
+  });
+
+
+// }
+
+
+
+// http://codepen.io/norman_pixelkings/pen/NNbqgG
+// https://stackoverflow.com/questions/38686650/slick-slides-on-pagination-hover
+
+
+
+
+
+/**
+Slick slider
+*/
+if( $('.responsive-slider').length ){
+    $('.responsive-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+    new WOW().init();
+
+})(jQuery);
