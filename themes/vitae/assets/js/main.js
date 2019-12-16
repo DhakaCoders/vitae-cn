7e87aa6318ccfe0a3cf434b43303c27acf2ef6a5
Rannojit <cserannojitsarkar22@gmail.com>
2019-12-17 01:49:38 +0600

    exchange-dark responsive done

--- themes/vitae/assets/js/main.js
+++ themes/vitae/assets/js/main.js
@@ -137,24 +137,15 @@ if( $('.exchangeSolutionSlider').length ){
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
             slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToScroll: 1,
           }
         },
         {
-          breakpoint: 480,
+          breakpoint: 768,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
+            dots: true,
           }
         }
         // You can unslick at a given breakpoint now by adding:
