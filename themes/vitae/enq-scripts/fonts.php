6c7e9cecf552579d7ba2e940a61acbcf093f31fe
Foridul Islam <foridulsumon@gmail.com>
2019-12-23 02:08:56 +0600

    acf plugin updated, fontawesome fix

--- themes/vitae/enq-scripts/fonts.php
+++ themes/vitae/enq-scripts/fonts.php
@@ -1,5 +1,5 @@
 <?php 
 wp_enqueue_style('google-poppins', 'https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap', array(), '1.0');
-wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/fonts/font-awesome/fontawesome.min.css', array(), '5.5.0');
+wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/fonts/font-awesome/font-awesome.css', array(), '5.5.0');
 wp_enqueue_style('cbv-fonts', get_template_directory_uri() . '/assets/fonts/custom-fonts.css', array(), '1.0.0');
 ?>
